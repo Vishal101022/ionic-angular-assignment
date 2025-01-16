3974e159bf00f98878ea643047b28c1e7b0c30e0
Vishal101022 <raivishal939@gmail.com>
2025-01-16 16:08:17 +0530

    :adhesive_bandage: Fix standalone error

--- resume-app/src/app/tab1/tab1.module.ts
+++ resume-app/src/app/tab1/tab1.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
-
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
 @NgModule({
@@ -11,8 +10,8 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
-    Tab1PageRoutingModule
+    Tab1PageRoutingModule,
+    Tab1Page
   ],
-  declarations: [Tab1Page]
 })
 export class Tab1PageModule {}
